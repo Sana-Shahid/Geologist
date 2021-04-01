d7bc020587c4694dcd0c4ed9a3dd3187a11b8c1d
Sana-Shahid <77516425+Sana-Shahid@users.noreply.github.com>
2021-04-01 19:48:37 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    stone1 = new Stone(500,100,50,50);
+    rubber1 = new Rubber(600,10,50);
+    iron1 = new Iron(100,200,50,50);
+
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    
+    plane.display();
+    hammer.display();
+    stone1.display();
+    rubber1.display();
+    iron1.display();
+
+    
+ 
+}
---
+++ stone.js
@@ -0,0 +1,37 @@
+class Stone {
+    constructor(x,y) {
+    var options = {
+        'density':4,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 150;
+      this.height = 40;
+      World.add(world, this.body);
+    };
+    
+    
+    
+    
+    display() 
+{
+    var stonepos = this.body.position
+    
+    push();
+    translate(stonepos.x, stonepos.y);
+    strokeWeight(3);
+    stroke('white')
+    fill('blue')
+    rectMode(CENTER)
+    rect(0, 0, this.width, this.height);
+    pop();
+}
+
+    
+}
+  
+
+
+
+
